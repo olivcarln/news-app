c043706fc0b3e750f4425eae098b918a82c25be9
mac <mac@Carle.local>
2025-10-09 13:40:43 +0700

    configuring an API service

---
+++ lib/models/news_response.dart
@@ -0,0 +1,26 @@
+import 'package:news_app/models/news_articles.dart';
+
+class NewsResponse {
+  final String status;
+  final int totalResults;
+  final List<NewsArticles> articles;
+
+  NewsResponse({
+    required this.status,
+    required this.totalResults,
+    required this.articles,
+  });
+
+  factory NewsResponse.fromJson(Map<String, dynamic> json) {
+    return NewsResponse(
+      status: json['status'] ?? '',
+      totalResults: json['totalResults'] ?? 0,
+      // kode yang digunakan untuk mengkonfersi data mentah 
+      // agar siap digunakan oleh aplikasi 
+      articles: (json['articles'] as List<dynamic>?)
+                 ?.map((article) => NewsArticles.fromJson(article))
+                .toList() ??
+          [],
+    );
+  }
+}
