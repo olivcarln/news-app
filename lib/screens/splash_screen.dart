31c88fce9c262c57956166842461b8f695efc590
mac <mac@Carle.local>
2025-10-23 10:48:17 +0700

    complete routing on the splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:news_app/routes/app_pages.dart';
 import 'package:news_app/utils/app_colors.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -42,7 +44,7 @@ class _SplashScreenState extends State<SplashScreen>
 
     // navigate to home screen after 3 seconds
     Future.delayed(Duration(seconds: 3), () {
-      // Get.offAllNamed(Routes.HOME);
+      Get.offAllNamed(Routes.HOME);
     });
   }
 
